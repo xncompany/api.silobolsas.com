8ab12fd5a4b3a67ec613c8e129a5fbe363391a6c
XN Developer <xn-developer>
2018-03-05 22:58:55 +0000

    routes/api.php removed + GET /user/{id}

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Land;
+use App\User;
+
+class UserController extends Controller
+{
+    /**
+     * Get User for a given id
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function getById($id) {
+        
+        return User::where('id', $id)->first();
+    }
+
+    /**
+     * List lands for a given User
+     *
+     * @param  int  $land
+     * @return Response
+     */
+    public function listLands($idUser) {
+        return Land::where('user', $idUser)->get();
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,31 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// ======= Users
 
-Route::get('/', function () {
+Route::get('/users/{id_user}', 'UserController@getById');
+Route::get('/users/{id_user}/lands', 'UserController@listLands');
 
-	$users = DB::connection('mysql')->select('SELECT * FROM users');
+// ======= Lands
 
-    return response()->json([
-	    'status' => 'Up & Running',
-	    'time' => time(),
-	    'mysql' => array('users' => $users),
-	    'github' => array('user' => 'xn-developer', 'password' => '00e8579911e947913fa6a5f5cf60abe7', 'cache_command' => "git config --global credential.helper 'cache --timeout=31536000'")
-	]);
-});
+Route::get('/lands', 'LandController@listLands');
+Route::post('/lands', 'LandController@createLand');
+Route::get('/lands/{id_land}/silobags', 'SilobagController@listSilobags');
+
+// ======= Silobags
+
+Route::post('/silobags', 'SilobagController@createSilobag');
+Route::get('/silobags/{id_silobag}', 'SilobagController@getSilobag');
+Route::put('/silobags/{id_silobag}', 'SilobagController@updateSilobag');
+Route::get('/silobags/{id_silobag}/devices', 'DeviceController@listDevices');
+
+// ======= Devices
+
+Route::get('/devices/{id_device}/metrics', 'MetricController@listMetrics');
+Route::get('/devices/{id_device}', 'DeviceController@getDevice');
+Route::put('/devices/{id_device}', 'DeviceController@updateDevice');
+Route::get('/devices/{id_device}/alerts', 'AlertController@listAlerts');
+
+// ======= Alerts
+
+Route::post('/alerts', 'AlertController@createAlert');
+Route::put('/alerts/{id_alert}', 'AlertController@updateAlert');
